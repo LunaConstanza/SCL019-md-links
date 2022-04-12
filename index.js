3424d57ef2cca4468b43313e8ca0172bc04cb43a
LunaConstanza <lunagonb@gmail.com>
2022-04-12 02:21:59 -0400

    proceso de lectura de directorio

--- index.js
+++ index.js
@@ -1,111 +1,27 @@
 const path = require('path');
 const fs = require('fs');
-const { arrayBuffer } = require('stream/consumers');
 
-//Funcion que retorna un array solo con lo archivos .md de un directorio (path)
+//Funcion que retorna los archivos.md de un directorio (path)
 const filterMDFiles = (pathToFilter) => {
   //LEER CARPETAS
-  //const entries = fs.readdir(pathToFilter, { withFileTypes: true });
-
   fs.readdir(pathToFilter, (err, files) => {
-
     if (err) {
       console.log(err.message);
     } else {
-
       let mdFiles = files.filter((element) => {
         if (path.extname(element) === ".md") {
           return element;
-        }
+        } //-------------------------
       });
-
       console.log(mdFiles);
 
-      //const recursiveFind = ( routePath) => {
-      //const foldercito = fs.readdirSync(routePath);
-      //foldercito.reduce( (prev, curr) => {
-      ////console.log(curr, fs.statSync(curr).isDirectory());
-      //const route = path.join(routePath, curr);
-      //if( fs.statSync(route).isDirectory() ) {
-      //console.log(route);
-      //console.log(prev) ;
-      //}
-      //} );
-      //files.forEach((elem) => {
-      //const route = path.join(routePath, elem);
-      //console.log(route, fs.statSync(route).isDirectory());
-      //if (fs.statSync(route).isDirectory()) {
-
-      //}
-      //})
-      //};
-
-      //recursiveFind(pathToFilter);
-
-      //let folders = files.filter((file) => {
-      //fs.lstat(file, (err, stats) => {
-      //if(err){
-      //console.log(err.message);
-      //};
-
-      //if( stats.isDirectory() ){
-      //console.log(file);
-      //return file;
-      //};
-      //})
-      //});
-      //console.log('FOLDERS:', folders);
-
-
-      //BUENISIMAAA ------------------
-      //-------------------------------------------------------------------------------------------------------
-      const getAllFiles = (dir) =>
-        fs.readdirSync(dir).reduce((files, file) => {
-          const name = path.join(dir, file);
-          const isDirectory = fs.statSync(name).isDirectory();
-          console.log('fileeees ', [...files])
-          return isDirectory ? [...files, ...getAllFiles(name)] : [...files, file];
-        }, []);
-
-      const filtraditos = getAllFiles(pathToFilter);
-
-      let mdRecursivos = filtraditos.filter((element) => {
-        if (path.extname(element) === ".md") {
-          return element;
-        }
-      });
-
-      console.log(mdRecursivos);
-      //-------------------------------------------------------------------------------------------------------
-
-      //const getAllFiles = ( dir ) => 
-      //fs.readdirSync(dir).reduce((files, file) => {
-      //const name = path.join(dir, file);
-      //let holaa = [];
-      //if( fs.statSync(name).isDirectory() ){
-      //holaa = [...files, ...getAllFiles(name)];
-      //} else {
-      //holaa = [...files, file];
-      //}
-
-      //holaa = holaa.map( (e) => {
-      //if ( path.extname(e) === ".md" ) {
-      //return element;
-      //}
-      //} );
-      ////console.log(holaa);
-      //return holaa;
-      //}, []);
-
-      //console.log('Archivos: ', getAllFiles(pathToFilter));
-      //BUENISIMAAA ------------------
     }
   });
 };
 
 const mdLinks = (inputPath, options) => {
 
-  let filesExtMD;
+  let filesDirectMD;
   if (fs.lstatSync(inputPath).isDirectory()) {
     // Si es directorio, revisamos existencias de archivos.md
     // filesExtMD = filterMDFiles(inputPath);
@@ -113,26 +29,27 @@ const mdLinks = (inputPath, options) => {
   } else {
     // Si es un archivo analizamos si es .md o no
     if (path.extname(inputPath) === ".md") {
-      filesExtMD = inputPath;
+      filesDirectMD = inputPath;
     }
     else {
       console.log("No existen archivos.md para analizar.");
     }
   };
-  console.log('nombre de archivo: ', filesExtMD);
+  console.log('nombre de archivo: ', filesDirectMD);
 
-  // Obtengo la data del archivo md y extraigo los links
-  let dataFiles = fs.readFileSync(filesExtMD, 'utf8');
+  // Obtengo la data del archivo.md y extraigo los links
+  let dataFiles = fs.readFileSync(filesDirectMD, 'utf8');
   const regExp = /(https?:\/\/)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/gi;
+
   let arrayLinks = [];
-  if (regExp.test(dataFiles) == true){
+  if (regExp.test(dataFiles) == true) {
     console.log("Existen links para analizar");
     arrayLinks = dataFiles.match(regExp);
   } else {
     console.log("No existen links para analizar");
   }
   console.log('array de links: ', arrayLinks);
-  
+
 
 
 };
