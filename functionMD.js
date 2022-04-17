3ac51e02b1d3f9736e23f35f6d7934c0133af097
LunaConstanza <lunagonb@gmail.com>
2022-04-17 01:49:59 -0400

    new promise | refact code

--- functionMD.js
+++ functionMD.js
@@ -1,41 +1,70 @@
 const path = require('path');
 const fs = require('fs');
+// const axios = require('axios').default;
+const fetch = require('node-fetch');
 const color = require('colors');
 
 //Función que lee directorio y retorna los archivos.md
 function filesMD(iPath) {
     const readDirectory = fs.readdirSync(iPath, 'utf8');
-    const arrayFiles = readDirectory.filter((file) => {
+    let arrayFiles = readDirectory.filter((file) => {
         if (path.extname(file) === ".md") {
             return file;
-        }                                                     // podria crear bucle con iteración(?)
-        // else if (path.extname(file) === "") {
-        //     if (fs.lstatSync(file).isDirectory()) {
-        //         return filesMD(file);
-        //     }
-        // }
+        }
+        // Queeee hago !? no funciona el bucle T.T ------------------------
+        // (fs.lstatSync(file).isDirectory()) ? filesMD(file) : bdv
+         
+        // } // Hasta aqui, lo de arriba no funciona :c ---------------------
     });
     return arrayFiles;
 };
 
 //Función que extrae los link de los archivos.md
-function arrayOfLinks(pathAbsolute) {
-    pathAbsolute.map(function (file) {
-
+function extractLinks(pathAbsolute) {
+    const arrayOfLinks = pathAbsolute.map(function (file) {
+        
         //expresión regular para hacer la comparación con links
         const regExp = /(https?:\/\/)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/gi;
-
+        
         let dataFiles = fs.readFileSync(file, 'utf8');        // Obtengo la data del archivo.md
-        let arrayLinks = [];
-        if (regExp.test(dataFiles) == true) {                 // extracción de links con regExp
-            arrayLinks = dataFiles.match(regExp);
-            console.log(color.bold.cyan('En "', file, '" sí existen links para analizar.'));
-            return arrayLinks;
+        if (regExp.test(dataFiles)) {                 // extracción de links con regExp
+            console.log(color.bold.cyan('En "', file, '" sí existen links para analizar. Son'));
+            dataLinks(dataFiles.match(regExp));
         } else {
             console.log(color.red('En "', file, '" no existen links para analizar.'));
         }
     });
 };
 
-exports.filesMD = filesMD;
-exports.arrayLinks = arrayOfLinks;
+const dataLinks = (links) => {
+    const objs = links.map((e) =>{
+        return fetch(e)
+        .then((response)=>{
+            return {
+                href: e,
+                // text: ,
+                // file: ,
+                status: response.status,
+                ok: (response.status===200) ? 'ok' : 'fail',
+            }
+        })
+        .catch((error)=>{
+            return {
+                href: e,
+                // text: ,
+                // file: ,
+                status: (error.status==='undefined') ? 'No existe status' : error.status,
+                ok: 'fail',
+            }
+        })
+    });
+    Promise.all(objs)
+    .then((response)=>{
+        console.log(response);
+    })
+}
+
+module.exports = {
+    filesMD,
+    extractLinks,
+};
--- index.js
+++ index.js
@@ -1,42 +1,71 @@
 const path = require('path');
 const fs = require('fs');
-const funMD = require('./functionMD.js');
+const { filesMD, extractLinks } = require('./functionMD');
 const color = require('colors');
 const gradient = require('gradient-string');
 
+const [termMethod, termPath, termVal1, termVal2 ] = process.argv.slice(2);
+
 const mdLinks = (inputPath, options) => {
-
-  let filesAbsolute;
-  if (fs.lstatSync(inputPath).isDirectory()) {      //si ruta es directorio
-    console.log(color.bold('Tu ruta es un directorio.'));
-    filesAbsolute = funMD.filesMD(inputPath);                             //Función para analizar directorio
-  } else {
-    if (path.extname(inputPath) === ".md") {        // Si ruta es archivo.md
-      console.log(color.bold('Tu ruta es un archivo.md'));
-      filesAbsolute = [inputPath];
+  return new Promise(function(res, rej){
+    if (!fs.existsSync(inputPath)){
+      rej('La ruta ingresada no es valida.')
     }
-    else {
-      console.log(color.bold.red("No existen archivos.md para analizar."));   //Si ruta NO es archivo.md
-    }
-  };
+    let filesAbsolute;
+    if (fs.lstatSync(inputPath).isDirectory()) {      //si ruta es directorio
+      console.log(color.bold('Tu ruta es un directorio.'));
+      filesAbsolute = filesMD(inputPath);             //Función para analizar directorio
+      console.log(gradient.cristal('Archivos para analizar:'), filesAbsolute);
+    } else {
+      if (path.extname(inputPath) === ".md") {        // Si ruta es archivo.md
+        console.log(color.bold('Tu ruta es un archivo.md'));
+        filesAbsolute = [inputPath];
+        console.log(gradient.cristal('Archivo para analizar:'), filesAbsolute);
+      }
+      else {
+        rej("La ruta ingresada no corresponde a un archivo Markdown.");   //Si ruta NO es archivo.md
+      }
+    };
+  
+    extractLinks(filesAbsolute);                      //Función para extraer links
 
-  console.log(gradient.cristal('Archivos para analizar:'), filesAbsolute);
-
-  funMD.arrayLinks(filesAbsolute);                      //Función para extraer links
-
+  });
 };
 
 
-//return new Promise( ( res, rej ) => {
-//} );
+// Validación de parámetros por terminal
+if (termMethod == 'mdLinks' && termPath && termVal1 == '--validate'){
+  mdLinks(termPath, termVal1)
+  .then(function(res){
 
-//UNIR RUTAS
-//console.log(path.join( __dirname, 'README.md' ));
+  })
+  .catch((rej) => {
+    console.log(color.bold.red(rej));
+  })
+}
+else if (termMethod == 'mdLinks' && termPath && termVal1 == '--stats'){
+  mdLinks(termPath, termVal1);
+}
+// else if (termMethod == 'mdLinks' && termPath && termVal1 == '--validate' && termVal2 == '--stats'||termMethod == 'mdLinks' && termPath && termVal1 == '--stats' && termVal2 == '--validate'){
+//   mdLinks(termPath, termVal1)
+// }
+else {
+  console.log('Faltan parametros para analizar.');
+}
 
-mdLinks('./')
-  //.then( (resolve) => {
-    //console.log(resolve);
+// mdLinks(parametros[2])
+//.then( (resolve) => {
+  //console.log(resolve);
   //} )
   //.catch( (err) => {
     //console.log(err);
-  //} );
+    //} );
+
+
+    
+    //return new Promise( ( res, rej ) => {
+    //} );
+    
+    //UNIR RUTAS
+    //console.log(path.join( __dirname, 'README.md' ));
+
